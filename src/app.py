271dfc39b05ddf58ee7fb40ec0f060835a2c14af
Jorge3127 <jorge.pedroza3127@gmail.com>
2025-02-17 19:27:01 +0000

    CODIGO AJUSTADO 5

--- src/app.py
+++ src/app.py
@@ -87,7 +87,7 @@ def index():
 
 if __name__ == "__main__":
     app.logger.info("Iniciando la aplicación Flask...")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     }
   ]
 }
